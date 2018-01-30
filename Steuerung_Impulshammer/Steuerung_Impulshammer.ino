91476d76b54add25f9dbe656bf1da060ad55683a
Unknown <lennart.stohrer@rwth-aachen.de>
2018-01-30 19:32:24 +0100

    LCD working

--- Steuerung_Impulshammer/Steuerung_Impulshammer.ino
+++ Steuerung_Impulshammer/Steuerung_Impulshammer.ino
@@ -28,32 +28,19 @@ int InputPotiC = 3; // PotiC --> Leistung
 //Pin Belegungen der Outputs
 int Hubmagnet = 3; // Gate des Mosfets an Pin 3 (PWM)
 
+int tic;
+
 
 void setup() {
-
+  //Setup KraM
   lcd.begin(16,2);
-  
-  lcd.setCursor(0,0);
-  lcd.print("Pulsdauer=");
-  lcd.setCursor(11,0);
-  lcd.print(Pulsdauer);
-  lcd.setCursor(14,0);
-  lcd.print("ms");
-
-  int leistung = Leistung*0.3922;
-  lcd.setCursor(0,1);
-  lcd.print("Leistung=");
-  lcd.setCursor(10,1);
-  lcd.print(leistung);
-  lcd.setCursor(14,1);
-  lcd.print("%");
-
-  
   Serial.begin(9600); 
   pinMode(InputTasterA,INPUT);
   pinMode(InputTasterB,INPUT);
   pinMode(Hubmagnet, OUTPUT);
-  
+
+  tic=millis();
+  lcd_print_static();
 }
 
 
@@ -62,7 +49,7 @@ void loop()
 {
 
 
-
+  lcd_print_dynamic();
 //Taster
   
   ZustandTasterA = digitalRead(InputTasterA);
@@ -75,28 +62,69 @@ void loop()
 //Potentiometer
 
   Pulsdauer = analogRead(InputPotiA);
-  Pulsdauer = map(Pulsdauer, 0, 1023, 20, 100); // Intervall der Pulsdauer: 20ms - 100ms
+  Pulsdauer = map(Pulsdauer, 0, 1014, 20, 100); // Intervall der Pulsdauer: 20ms - 100ms
   //Serial.println(Pulsdauer);
-  delay(20);
+  //delay(20);
 
-  Wiederholfrequenz = analogRead(InputPotiA);
+  Wiederholfrequenz = analogRead(InputPotiB);
   Wiederholfrequenz = map(Wiederholfrequenz, 0, 1023, 200, 2000); // Intervall der Wiederholfrequenz: 20ms - 2000ms
   //Serial.println(Leistung);
-  delay(20);
+  //delay(20);
 
-  Leistung = analogRead(InputPotiA);
-  Leistung = map(Leistung, 0, 1023, 150, 255); // Intervall der PWM: 150 - 255
-  //Serial.println(Leistung);
-  delay(20);
+  Leistung = analogRead(InputPotiC);
+  Serial.println(Leistung);
+  Leistung = map(Leistung, 0, 1010, 150, 255); // Intervall der PWM: 150 - 255
+  
+  //delay(20);
 
 
-
+/*
   analogWrite(Hubmagnet, Leistung);
   delay(Pulsdauer);
   analogWrite(Hubmagnet, Leistung);
-  delay(Wiederholfrequenz);
+  delay(Wiederholfrequenz);*/
 
 }
+
+
+void lcd_print_static()
+{
+  lcd.setCursor(0,0);
+  lcd.print("Pulsdauer=");
+  lcd.setCursor(14,0);
+  lcd.print("ms");
+  lcd.setCursor(0,1);
+  lcd.print("Leistung=");
+  lcd.setCursor(14,1);
+  lcd.print("%");
+}
+void lcd_print_dynamic()
+{
+  int toc=millis();
+  if((toc-tic)>100)
+  {
+    lcd.setCursor(10,0);
+    lcd.print("    ");
+
+    lcd.setCursor(10,1);
+    lcd.print("    ");
+    tic=millis();
+  }
+ 
+  
+  lcd.setCursor(10,0);
+  lcd.print(Pulsdauer);
+  
+  double leistung=0.393*Leistung;
+  
+  //int leistung = Leistung;
+  
+  lcd.setCursor(10,1);
+  lcd.print((int)leistung);
+  
+  
+}
+
 
 
 
